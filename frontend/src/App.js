c20caeeef395b461a4f25002ef6a6a979a9fc079
Jan Huber <hurj@zhaw.ch>
2021-03-12 08:01:50 +0100

    Implemented Simple Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import './normalize.css';
+import './LoginService.js';
 
 
 function App() {
   return (
     <div className="App">
-
         <Introduction></Introduction>
-
+        <LoginForm></LoginForm>
+        <RotatingReactLogo></RotatingReactLogo>
     </div>
   );
 }
@@ -17,8 +18,34 @@ function Introduction() {
     return (
         <div className="Introduction">
             <h1>Hey there! Welcome to Strategy Game</h1>
-            <img src={logo} className="App-logo" alt="logo" />
         </div>
+    );
+}
+
+function LoginForm() {
+    return (
+
+        <div>
+
+            <h2>Login to play</h2>
+
+            <form id="login-form" action="/play.html">
+                <label htmlFor="input-username">Username:</label><br/>
+                <input type="text" id="username-input" name="username" defaultValue="Demoplayer" required/><br/>
+                <label htmlFor="input-password">Password:</label><br/>
+                <input type="password" id="password-input" name="password" defaultValue="1234" required/><br/><br/>
+                <input type="submit" value="Login" />
+            </form>
+
+        </div>
+    );
+}
+
+
+
+function RotatingReactLogo() {
+    return (
+        <img src={logo} className="App-logo" alt="logo" />
     );
 }
 
---
+++ frontend/src/LoginService.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let loginForm = document.getElementById("login-form");
+    let usernameInput = document.getElementById("username-input");
+    let passwordInput = document.getElementById("password-input");
+
+    loginForm.addEventListener("submit", event => {
+            event.preventDefault();
+            fetch("/login", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({username: usernameInput.value, password: passwordInput.value}),
+                credentials: "include"
+            }).then(function (res) {alert(res);});
+    });
+
+});// interactive
+
