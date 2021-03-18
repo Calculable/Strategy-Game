bd6884af96c4c46cfbf3507584e29d3ca0fc423b
Denis Nauli <denis.nauli@ost.ch>
2021-03-18 11:45:39 +0000

    Backend init

--- service/api/models.py
+++ service/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    number_of_gold = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
--- service/api/views.py
+++ service/api/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Player
+from .serializers import PlayerSerializer
 
-# Create your views here.
-def main(request):
-    return HttpResponse("Hello")
+
+class PlayerView(generics.ListAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+@api_view(['GET', 'POST'])
+def players_list(request):
+    if request.method == 'GET':
+        data = Player.objects.all()
+
+        serializer = PlayerSerializer(data, context={'request': request}, many=True)
+
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = PlayerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
